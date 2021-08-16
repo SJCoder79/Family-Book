9ba4b184f1dcc842a0ee08818938f46dad6640e8
suhanibansal870 <83600598+suhanibansal870@users.noreply.github.com>
2021-08-15 17:44:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ var i = 0;
 
 function update(){
 
-var array_length = images.length - 1;
-var updated_image = images[i];
-var updated_text = names[i];
+  var array_length = images.length - 1;
+  var updated_image = images[i];
+  var updated_text = names[i];
 
-document.getElementById("family_book_image").src = updated_image;
-document.getElementById("name").innerHTML = updated_text;
+  document.getElementById("family_book_image").src = updated_image;
+  document.getElementById("name").innerHTML = updated_text;
 
-i++;
+  i++;
 
 
-if(i>array_length){
-i = 0;
+  if(i>array_length){
+  i = 0;
                 
-}
+  }
 }
