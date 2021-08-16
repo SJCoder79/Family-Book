e1edb76291f3fa2eaa56ee29ea3f87d84c46cac4
suhanibansal870 <83600598+suhanibansal870@users.noreply.github.com>
2021-08-15 17:45:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var images = ["https://as2.ftcdn.net/jpg/01/15/47/87/500_F_115478791_GG0yIcVA8Mhe6MAXEvgcfyL7JtuVfYvm.jpg", "https://lh3.googleusercontent.com/proxy/KCzVb0mD-DWJmjwakjKD8QLLqEE7Y9-Ztf7Hv4tsh3a_6GWonvfn_PY4Dz0LNxIuWJCWpV2f1Wu5sb5adnHQcaVcR3skeJf7OvFExLk4ug", "https://previews.123rf.com/images/yupiramos/yupiramos1712/yupiramos171207137/91230755-young-girl-kid-character-standing-cartoon-vector-illustration.jpg"];
-var names = ["Mom", "Dad", "Me"];
+var images = ["https://as2.ftcdn.net/jpg/01/15/47/87/500_F_115478791_GG0yIcVA8Mhe6MAXEvgcfyL7JtuVfYvm.jpg", "https://lh3.googleusercontent.com/proxy/KCzVb0mD-DWJmjwakjKD8QLLqEE7Y9-Ztf7Hv4tsh3a_6GWonvfn_PY4Dz0LNxIuWJCWpV2f1Wu5sb5adnHQcaVcR3skeJf7OvFExLk4ug", "https://previews.123rf.com/images/yupiramos/yupiramos1712/yupiramos171207137/91230755-young-girl-kid-character-standing-cartoon-vector-illustration.jpg", "family.png"];
+var names = ["Mom", "Dad", "Me", "Us"];
 var i = 0;
 
 function update(){
